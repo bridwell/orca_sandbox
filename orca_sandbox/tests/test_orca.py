3293da1dcac605e3221c9c35bd1e168459a71850
scott bridwell <bridwell@gmail.com>
2016-12-05 11:30:52 -0700

    add tests for evaluating table wrappers

--- orca_sandbox/tests/test_orca.py
+++ orca_sandbox/tests/test_orca.py
@@ -106,7 +106,7 @@ def test_callbacks(my_func2):
     assert cb(10) == 20
 
 
-def test_columns():
+def test_column_wrapper():
     """
     Test creating columns.
 
@@ -141,3 +141,94 @@ def test_columns():
     assert len(a) == 2
     assert len(a['something']) == 2
     assert len(a['something_else']) == 1
+
+
+def test_table_wrappper():
+    orca.clear_all()
+    row_count = 5
+
+    # wrap and attach a column/series
+    orca.add_column('ones', pd.Series(np.ones(row_count)), attach_to='a_df')
+
+    # wrap a function that returns a series and attach it
+    def twos():
+        return pd.Series(np.ones(row_count)) * 2
+    orca.add_column('twos', twos, attach_to='a_df')
+
+    # wrap a function that returns a data frame
+    df = pd.DataFrame({
+        'threes': np.ones(row_count) * 3,
+        'fours': np.ones(row_count) * 4,
+    })
+    orca.add_table('blah_df', df, attach_to='a_df')
+
+    # evaluate the stuff we've wrapped
+    assert (orca.eval_injectable('ones') == 1).all()
+    assert (orca.eval_injectable('twos') == 2).all()
+
+    blah = orca.eval_injectable('blah_df')
+    assert len(blah) == row_count
+    assert 'threes' in blah.local_columns
+    assert 'fours' in blah.local_columns
+    assert blah.attached_columns == []
+    assert (blah.index.values == np.arange(row_count)).all()
+    assert (blah.threes == 3).all()
+    assert (blah['fours'] == 4).all()
+
+    # wrap a target data frame that we are attaching stuff to
+    target = pd.DataFrame({
+        'fives': np.ones(row_count) * 5,
+        'sixes': np.ones(row_count) * 6,
+    })
+    orca.add_table('a_df', target)
+
+    # evaluate the target table
+    tab = orca.eval_injectable('a_df')
+    expected = {
+        'ones': 1,
+        'twos': 2,
+        'threes': 3,
+        'fours': 4,
+        'fives': 5,
+        'sixes': 6
+    }
+
+    for c in expected.keys():
+        assert c in tab.columns
+    for c in ['fives', 'sixes']:
+        assert c in tab.local_columns
+    for c in ['ones', 'twos', 'threes', 'fours']:
+        assert c in tab.attached_columns
+
+    # check grabbing all attached columns
+    df_all = tab.to_frame()
+    for c in expected.keys():
+        assert c in df_all.columns
+    for k, v in expected.items():
+        assert (df_all[k] == v).all()
+
+    # check grabbing just local columns
+    c = ['fives', 'sixes']
+    df_local = tab.to_frame(c)
+    assert (df_local.columns == c).all()
+    assert (df_local.fives == 5).all()
+    assert (df_local['sixes'] == 6).all()
+
+    # check grabbing just attached
+    c = ['ones', 'threes']
+    df_a = tab.to_frame(c)
+    assert (df_a.columns == c).all()
+    assert (df_a.ones == 1).all()
+    assert (df_a.threes == 3).all()
+
+    # check grabbing both local and attached
+    c = ['fours', 'twos', 'sixes']
+    df_a = tab.to_frame(c)
+    assert (df_a.columns == c).all()
+    assert (df_a.fours == 4).all()
+    assert (df_a.twos == 2).all()
+    assert (df_a.sixes == 6).all()
+
+    # test missing columns
+    with pytest.raises(ValueError):
+        tab.to_frame(['zeros', 'ones', 'twos'])
